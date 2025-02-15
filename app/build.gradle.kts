c77633c22842075a07d043ddc48e0a9ca13df416
shakedS10 <shaked.shvartz@msmail.ariel.ac.il>
2025-02-15 19:46:37 +0200

    added UI test for newRoom WOW :)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,7 +83,11 @@ dependencies {
     debugImplementation(libs.ui.tooling)
     debugImplementation(libs.ui.test.manifest)
     implementation("com.airbnb.android:lottie:5.2.0")
-
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation ("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation ("androidx.test:runner:1.5.2")
+    androidTestImplementation ("androidx.test:rules:1.5.0")
+    androidTestImplementation ("androidx.test.espresso:espresso-intents:3.5.1")
     // Imgur for uploading an image
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
---
+++ app/src/androidTest/java/com/example/bettertogether/NewRoomActivityTest.java
@@ -0,0 +1,34 @@
+package com.example.bettertogether;
+
+import static androidx.test.espresso.Espresso.onView;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.assertion.ViewAssertions;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.example.bettertogether.NewRoomActivity;
+import com.example.bettertogether.R;
+
+@RunWith(AndroidJUnit4.class)
+public class NewRoomActivityTest {
+
+    @Test
+    public void testVisibilityOfCodeInputWhenCheckboxChecked() {
+        // Launch the activity manually
+        ActivityScenario<NewRoomActivity> scenario = ActivityScenario.launch(NewRoomActivity.class);
+
+        // Check visibility
+        onView(ViewMatchers.withId(R.id.form_code_input))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+
+        onView(ViewMatchers.withId(R.id.form_checkbox_public))
+                .perform(ViewActions.click());
+
+        onView(ViewMatchers.withId(R.id.form_code_input))
+                .check(ViewAssertions.matches(ViewMatchers.withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
+    }
+}
